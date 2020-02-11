c79cd02d1b79406849751931f6b9bb0540bbded2
toxa16 <toxa16@gmail.com>
2020-02-11 22:10:08 +0200

    setting document title

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Navbar from '../components/navbar';
@@ -6,6 +7,10 @@ import Footer from '../components/footer';
 export default function App({ Component, pageProps }) {
   return (
     <div className="d-flex flex-column min-vh-100">
+      <Head>
+        <title>Next.js SSG vs SSR Demo</title>
+      </Head>
+
       <Navbar />
 
       <main className="container flex-grow-1 my-5">
