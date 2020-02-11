d86b37f93bf2ca111dafb7eb2597c8103c62d2ad
toxa16 <toxa16@gmail.com>
2020-02-11 21:33:03 +0200

    adding homepage content

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
 function Homepage() {
   return (
     <div>
-      <h1>Homepage</h1>
+      <h1 className="mb-4">Homepage</h1>
+
+      <div className="row">
+        <div className="col-12 col-lg-10 col-xl-8">
+          <p className="font-weight-bold">
+            Welcome to the SSG vs SSR Next.js demo app!
+          </p>
+
+          <p>
+            You are now at the app <b>homepage</b>. This page doesn't fetch 
+            any back-end data and represents a <i>lightweight initial 
+            page</i> of a Next.js app.
+          </p>
+
+          <p>
+            This is a demo application for the <a href="https://antoncodes.com">Next.js 
+            SSG vs SSR article series</a> from the AntonCodes blog.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
+
+/*Homepage.getInitialProps = () => {
+  return {};
+}*/
 
 export default Homepage;
