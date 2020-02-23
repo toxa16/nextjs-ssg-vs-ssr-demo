bf1736a87377fc98b9cc5c74c3df926f731fdbc9
toxa16 <toxa16@gmail.com>
2020-02-23 20:07:07 +0200

    init blog ssg & ssr charts

--- pages/metrics/index.js
+++ pages/metrics/index.js
@@ -51,6 +51,56 @@ const homepageSSRData = {
   labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
 };
 
+// blog SSG metrics data
+const blogSSGData = {
+  datasets: [
+    {
+      label: 'DOMContentLoad',
+      data: [26, 55, 36, 43, 54, 35, 60, 40, 45, 49],
+      borderColor: 'blue',
+      pointBackgroundColor: 'blue',
+    },
+    {
+      label: 'Load',
+      data: [410, 364, 390, 381, 484, 401, 417, 425, 390, 387],
+      borderColor: 'red',
+      pointBackgroundColor: 'red',
+    },
+    {
+      label: 'Finish',
+      data: [1430, 1380, 1390, 1390, 1490, 1400, 1420, 1430, 1390, 1390],
+      borderColor: 'gray',
+      pointBackgroundColor: 'gray',
+    },
+  ],
+  labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+};
+
+// blog SSR metrics data
+const blogSSRData = {
+  datasets: [
+    {
+      label: 'DOMContentLoad',
+      data: [1.08, 1.05, 1.05, 1.05, 1.03, 1.06, 1.12, 1.05, 1.05, 1.05],
+      borderColor: 'blue',
+      pointBackgroundColor: 'blue',
+    },
+    {
+      label: 'Load',
+      data: [1.47, 1.40, 1.40, 1.38, 1.45, 1.43, 1.42, 1.44, 1.43, 1.37],
+      borderColor: 'red',
+      pointBackgroundColor: 'red',
+    },
+    {
+      label: 'Finish',
+      data: [1.49, 1.42, 1.41, 1.39, 1.47, 1.44, 1.44, 1.45, 1.45, 1.39],
+      borderColor: 'gray',
+      pointBackgroundColor: 'gray',
+    },
+  ],
+  labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+};
+
 // general chart options
 const options = {
   animation: false,
@@ -60,21 +110,25 @@ const options = {
       fill: false,
     },
   },
-  scales: {
-    yAxes: [{
-      scaleLabel: {
-        display: true,
-        labelString: 'Time, ms',
-      },
-    }],
-    xAxes: [{
-      scaleLabel: {
-        display: true,
-        labelString: 'Measurement Index',
-      },
-    }],
-  },
 };
+const xAxes = [{
+  scaleLabel: {
+    display: true,
+    labelString: 'Measurement Index',
+  },
+}];
+const yAxesS = [{
+  scaleLabel: {
+    display: true,
+    labelString: 'Time, s',
+  },
+}];
+const yAxesMs = [{
+  scaleLabel: {
+    display: true,
+    labelString: 'Time, ms',
+  },
+}];
 
 /**
  * "/metrics" page component.
@@ -90,6 +144,10 @@ export default function Metrics() {
           display: true,
           position: 'top',
         },
+        scales: {
+          xAxes,
+          yAxes: yAxesMs,
+        },
         ...options,
       },
     });
@@ -103,6 +161,44 @@ export default function Metrics() {
           display: true,
           position: 'top',
         },
+        scales: {
+          xAxes,
+          yAxes: yAxesMs,
+        },
+        ...options,
+      },
+    });
+
+    const blogSSGChart = new Chart('blogSSGChart', {
+      type: 'line',
+      data: blogSSGData,
+      options: {
+        title: {
+          text: 'Blog SSG',
+          display: true,
+          position: 'top',
+        },
+        scales: {
+          xAxes,
+          yAxes: yAxesMs,
+        },
+        ...options,
+      },
+    });
+
+    const blogSSRChart = new Chart('blogSSRChart', {
+      type: 'line',
+      data: blogSSRData,
+      options: {
+        title: {
+          text: 'Blog SSR',
+          display: true,
+          position: 'top',
+        },
+        scales: {
+          xAxes,
+          yAxes: yAxesS,
+        },
         ...options,
       },
     });
@@ -111,6 +207,8 @@ export default function Metrics() {
     return () => {
       homepageSSGChart.destroy();
       homepageSSRChart.destroy();
+      blogSSGChart.destroy();
+      blogSSRChart.destroy();
     };
   });
 
@@ -126,6 +224,16 @@ export default function Metrics() {
       <div className="mb-5">
         <h2 className="h6">Homepage SSR:</h2>
         <canvas id="homepageSSRChart"></canvas>
+      </div>
+
+      <div className="mb-5">
+        <h2 className="h6">Blog SSG:</h2>
+        <canvas id="blogSSGChart"></canvas>
+      </div>
+
+      <div className="mb-5">
+        <h2 className="h6">Blog SSR:</h2>
+        <canvas id="blogSSRChart"></canvas>
       </div>
     </div>
   );
